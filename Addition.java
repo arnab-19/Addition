435e3fa054780036b08b1025d2de0009c71834a4
ARNAB RAHA <84772250+arnab-19@users.noreply.github.com>
2021-05-28 14:36:41 +0530

    Add files via upload

---
+++ Addition.java
@@ -0,0 +1,10 @@
+public class Addition
+{
+	public static void main(String[] args) 
+	{
+	    int a = 4;
+	    int b = 5;
+	    int c = a + b;
+		System.out.println("Sum of " + a + " and " + b + " is " + c);
+	}
+}
---
+++ Dna.java
@@ -0,0 +1,8 @@
+public class Dna
+{
+	public static void main(String[] args) 
+	{
+	    String dna = "ATACGATACAA";
+	    System.out.println(dna);
+	}
+}
